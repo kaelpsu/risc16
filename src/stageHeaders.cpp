06804b5a35b667a47e06310292b719c833c7421c
Kael Paraguassu <kaelparaguassu@hotmail.com>
2025-05-16 13:57:25 -0300

    adicionando entradas e saidas restantes nos estágios de pipeline

--- src/stageHeaders.cpp
+++ src/stageHeaders.cpp
@@ -10,8 +10,9 @@
 // IF Stage
 SC_MODULE(IFStage) {
     sc_in<bool> clk, rst_n;
-    // talvez pc e pc_next sejam signals
-    sc_in<sc_uint<16>> pc, pc_next, pc_src, pc_branch;
+    sc_in<sc_uint<16>>pc_branch;
+    sc_in<bool> pc_src;
+    sc_signal<sc_uint<16>> pc, pc_next;
     sc_out<sc_uint<16>> pc_out, instruction;
 
     memory *instruction_memory;
@@ -23,7 +24,7 @@ SC_MODULE(IFStage) {
             pc.write(pc_next.read());
     }
 
-    void pc_next_logic() {
+    void pc_mux() {
         switch (pc_src)
         {
         case 0b0: // PC + 2
@@ -41,42 +42,60 @@ SC_MODULE(IFStage) {
     void somador() {
         pc_out.write(pc.read() + 2);
     }
+    
+    void debug_signals() {
+        std::cout << "[IF]----------------------------------------------------------------------------" << std::endl;
+        std::cout << "PC: " << pc.read().to_string(SC_BIN) << std::endl;
+        std::cout << "PC Next: " << pc_next.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Instruction: " << instruction.read().to_string(SC_BIN) << std::endl;
+        std::cout << "--------------------------------------------------------------------------------" << std::endl;
+    }
 
     SC_CTOR(IFStage) {
+        std::cout << "Carregando memória de instruções..." << std::endl;
         instruction_memory = new memory("instruction_memory");
         instruction_memory->load_memory("../test_imemory.bin");
 
         SC_METHOD(pc_process);
         sensitive << clk.pos();
-        SC_METHOD(pc_next_logic);
-        sensitive << clk.pos();
         SC_METHOD(somador);
         sensitive << clk.pos();
-
-        instruction_memory->clk(clk);
-        instruction_memory->rst_n(rst_n);
-        instruction_memory->address(pc_out);
-
-        sc_signal<sc_uint<16>> null_data;
+        SC_METHOD(pc_mux);
+        sensitive << clk.pos();
+        
+        sc_signal<bool> rst_mem;
         sc_signal<bool> write_enable;
-        write_enable.write(false);
+        sc_signal<sc_uint<16>> null_data;
+        
+        rst_mem.write(true); // desativa reset da memoria
+        write_enable.write(false); // desativa escrita na memoria de instruções
+        null_data.write(0); // dado nulo para escrita na memória de instruções
+        
+        instruction_memory->clk(clk);
+        instruction_memory->rst_n(rst_mem);
+        instruction_memory->address(pc);
         instruction_memory->write_enable(write_enable);
         instruction_memory->write_data(null_data);
         instruction_memory->read_data(instruction);
+
+        SC_METHOD(debug_signals);
+        sensitive << clk.pos();
     }
 };
 
 // ID Stage
 SC_MODULE(IDStage) {
-    sc_in<bool> clk;
-    sc_in<sc_uint<16>> instruction, reg_write, write_back_data, dest_reg_back;
+    sc_in<bool> clk, rst_n;
+    sc_in<sc_uint<16>> instruction, write_back_data;
+    sc_in<sc_uint<3>> dest_reg_back;
+    sc_in<bool> imm_source, reg_write;
 
-    // talvez sejam signals
-    sc_in<sc_uint<4>> op;
-    sc_in<sc_uint<3>> source_reg1, source_reg2, dest_reg, raw_imm, imm_source;
+    sc_signal<sc_uint<4>> op;
+    sc_signal<sc_uint<3>> source_reg1, source_reg2, dest_reg;
+    sc_signal<sc_uint<12>> raw_imm;
 
     sc_out<sc_uint<16>> read_data1, read_data2, immediate;
-    sc_out<bool> imm_source;
+    sc_out<sc_uint<4>> op_out;
 
     regfile *regfile_inst;
     signext *sign_extender;
@@ -88,10 +107,28 @@ SC_MODULE(IDStage) {
         source_reg1.write(instr.range(8, 6));
         // mudar depois pra ser responsabilidade do controle
         source_reg2.write( (op.read() == 0b1000) ? instr.range(11, 9) : instr.range(5, 3));
-        immediate.write(instr.range(11, 0));
+        raw_imm.write(instr.range(11, 0));
+    }
+
+    void debug_signals() {
+        std::cout << "[ID]----------------------------------------------------------------------------" << std::endl;
+        std::cout << "Instruction: " << instruction.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Op: " << op.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Source Reg 1: " << source_reg1.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Source Reg 2: " << source_reg2.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Dest Reg Out: " << dest_reg.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Immediate: " << immediate.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Read Data 1: " << read_data1.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Read Data 2: " << read_data2.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Dest Reg Back: " << dest_reg_back.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Write Back Data: " << write_back_data.read().to_string(SC_BIN) << std::endl;
+        std::cout << "--------------------------------------------------------------------------------" << std::endl;
     }
 
     SC_CTOR(IDStage) {
+        SC_METHOD(decode_instr);
+        sensitive << clk.pos();
+        
         // Módulo: Banco de Registradores
         regfile_inst = new regfile("regfile");
         regfile_inst->clk(clk);
@@ -103,20 +140,21 @@ SC_MODULE(IDStage) {
         regfile_inst->write_data(write_back_data);
         regfile_inst->read_data1(read_data1);
         regfile_inst->read_data2(read_data2);
-
+        
         // Módulo: Sign Extender
         sign_extender = new signext("signext");
         sign_extender->raw_src(raw_imm);
         sign_extender->imm_source(imm_source);
         sign_extender->immediate(immediate);
 
-        SC_METHOD(decode_instr);
-        sensitive << instruction;
+        SC_METHOD(debug_signals);
+        sensitive << clk.pos();
     }
 };
 
 // EX Stage
 SC_MODULE(EXStage) {
+    sc_in<bool> clk, rst_n;
     sc_in<sc_uint<3>> ula_op;
     sc_in<sc_uint<16>> read_data1, read_data2, immediate, pc_in;
     sc_in<bool> ula_src;
@@ -137,23 +175,41 @@ SC_MODULE(EXStage) {
         pc_branch.write(pc_in.read() + 2 + (immediate.read() << 1));
     }
 
+    void debug_signals(){
+        std::cout << "[EX]----------------------------------------------------------------------------" << std::endl;
+        std::cout << "PC: " << pc_in.read().to_string(SC_BIN) << std::endl;
+        std::cout << "ULA Op: " << ula_op.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Read Data 1: " << read_data1.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Read Data 2: " << read_data2.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Immediate: " << immediate.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Src 2 Signal: " << src2_signal.read().to_string(SC_BIN) << std::endl;
+        std::cout << "ULA Result: " << ula_result.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Zero: " << zero.read() << std::endl;
+        std::cout << "Negative: " << negative.read() << std::endl;
+        std::cout << "PC Branch: " << pc_branch.read().to_string(SC_BIN) << std::endl;
+        std::cout << "--------------------------------------------------------------------------------" << std::endl;
+    }
+
     SC_CTOR(EXStage) {
-        ula_inst = new ula("ula_inst");
-
-        // Connect inputs
-        ula_inst->ula_op(ula_op);
-        ula_inst->src1(read_data1);
-        ula_inst->src2(src2_signal);
-
-        // Connect outputs
-        ula_inst->ula_result(ula_result);
-        ula_inst->zero(zero);
-        ula_inst->negative(negative);
-
+        
         // Multiplexador para src2
         SC_METHOD(select_src2);
         sensitive << read_data1 << read_data2 << immediate << ula_src;
         SC_METHOD(pc_calc);
+        sensitive << clk.pos();
+        
+        ula_inst = new ula("ula_inst");
+        // Connect inputs
+        ula_inst->ula_op(ula_op);
+        ula_inst->src1(read_data1);
+        ula_inst->src2(src2_signal);
+        
+        // Connect outputs
+        ula_inst->ula_result(ula_result);
+        ula_inst->zero(zero);
+        ula_inst->negative(negative);
+        
+        SC_METHOD(debug_signals);
         sensitive << clk.pos();
     }
 };
@@ -162,7 +218,9 @@ SC_MODULE(EXStage) {
 // MEM Stage
 SC_MODULE(MEMStage) {
     sc_in<bool> clk;
-    sc_in<bool> rst_mem;
+
+    sc_signal<bool> rst_mem;
+
     sc_in<bool> mem_write;
     sc_in<sc_uint<16>> ula_result, write_data;
 
@@ -170,39 +228,60 @@ SC_MODULE(MEMStage) {
 
     memory *data_memory;
 
+    void debug_signals() {
+        std::cout << "[MEM]---------------------------------------------------------------------------" << std::endl;
+        std::cout << "ULA Result: " << ula_result.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Write Data: " << write_data.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Mem Read: " << mem_read.read().to_string(SC_BIN) << std::endl;
+        std::cout << "--------------------------------------------------------------------------------" << std::endl;
+    }
+
     SC_CTOR(MEMStage) {
-        // colocar no modulo de cpu
+        
+        // Reset da memória de dados
+        rst_mem.write(true); // desativa reset da memoria
+
+        std::cout << "Carregando memória de dados..." << std::endl;
         data_memory = new memory("data_memory");
         data_memory->load_memory("../test_dmemory.bin");
-
+        
         data_memory->clk(clk);
         data_memory->rst_n(rst_mem);
         data_memory->write_enable(mem_write);
         data_memory->address(ula_result);
         data_memory->write_data(write_data);
         data_memory->read_data(mem_read);
+
+        SC_METHOD(debug_signals);
+        sensitive << clk.pos();
     }
 };
 
 // WB Stage
 SC_MODULE(WBStage) {
+    sc_in<bool> clk;
     sc_in<bool> mem_to_reg;
-    sc_in<sc_uint<16>> ula_result, mem_read, write_back_reg_in;
+    sc_in<sc_uint<16>> ula_result, mem_read;
 
-    sc_out<sc_uint<16>> write_back_data, write_back_reg_out;
+    sc_out<sc_uint<16>> write_back_data;
 
     void wb_mux() {
         write_back_data.write(mem_to_reg.read() ? mem_read.read() : ula_result.read());
     }
 
-    void write_back() {
-        write_back_reg_out.write(write_back_reg_in.read());
+    void debug_signals(){
+        std::cout << "[WB]----------------------------------------------------------------------------" << std::endl;
+        std::cout << "Mem To Reg: " << mem_to_reg.read() << std::endl;
+        std::cout << "ULA Result: " << ula_result.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Mem Read: " << mem_read.read().to_string(SC_BIN) << std::endl;
+        std::cout << "Write Back Data: " << write_back_data.read().to_string(SC_BIN) << std::endl;
+        std::cout << "--------------------------------------------------------------------------------" << std::endl;
     }
 
     SC_CTOR(WBStage) {
         SC_METHOD(wb_mux);
         sensitive << mem_to_reg << ula_result << mem_read;
-        SC_METHOD(write_back);
+        SC_METHOD(debug_signals);
         sensitive << clk.pos();
     }
 };
